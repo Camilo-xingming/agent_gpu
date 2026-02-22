15cc5429f970268370adbf482b9a3327f702f066
ssql2014 <ssql2014@gmail.com>
2026-02-22 12:40:41 -0800

    fix: iverilog compile compatibility for dual_issue_scheduler and memory_coalescing_unit (#165)
    - dual_issue_scheduler.v: replace 2D array indexing `inst_window[g][11:7]`
      with intermediate wire + `+:` part-select (iverilog-compatible)
    - memory_coalescing_unit.v: split `line_data[var][var +: W]` double-index
      into temp reg + single part-select
    - memory_coalescing_unit.v: add missing ADDR_WIDTH parameter to
      warp_memory_unit module
    
    Closes #165

--- rtl/dual_issue_scheduler.v
+++ rtl/dual_issue_scheduler.v
@@ -305,9 +305,10 @@ module ilp_analyzer #(
     generate
         genvar g;
         for (g = 0; g < WINDOW_SIZE; g = g + 1) begin : gen_decode
-            assign rd[g]  = inst_window[g][11:7];
-            assign rs1[g] = inst_window[g][19:15];
-            assign rs2[g] = inst_window[g][24:20];
+            wire [INST_WIDTH-1:0] inst_g = inst_window[g*INST_WIDTH +: INST_WIDTH];
+            assign rd[g]  = inst_g[11:7];
+            assign rs1[g] = inst_g[19:15];
+            assign rs2[g] = inst_g[24:20];
         end
     endgenerate
 
